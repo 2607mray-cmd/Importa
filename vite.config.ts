0586b5b77f1bfb276e9c8e10dcffb92e66490c26
2607mray-cmd <2607mray@gmail.com>
2025-12-02 01:26:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/manish-tea-traders/',
+    base: '/Importa/',
     server: {
       port: 3000,
       host: '0.0.0.0',
